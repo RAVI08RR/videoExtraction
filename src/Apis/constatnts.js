7deffa5b067adfef9d791259ad5f1ba8698f624f
RAVI08RR <ravisoni08rrrr@gmail.com>
2025-01-31 15:57:50 +0530

    API integration done on faceMatch result page and result teams page

--- src/Apis/constatnts.js
+++ src/Apis/constatnts.js
@@ -1,3 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === "development"
-  ? "http://183.83.216.29:7777/api" // Directly use API in development
-  : "/api"; // Use Vercel proxy in production
+export const BASE_URL = "http://183.83.216.29:7777/api";
